acda78378cebb80db585e37d4059da04a8a2bdff
Kevin <37316637+71Kevin@users.noreply.github.com>
2025-04-08 16:09:42 -0300

    Update version.py

--- version.py
+++ version.py
@@ -7,7 +7,6 @@
 
 import re
 import time
-
 import json5
 import httpx
 import lxml.html
@@ -17,14 +16,7 @@ PLAY_STORE_URL = {
     "JP": "https://play.google.com/store/apps/details?id=com.aniplex.fategrandorder",
     "KR": "https://play.google.com/store/apps/details?id=com.netmarble.fgok",
     "TW": "https://play.google.com/store/apps/details?id=com.xiaomeng.fategrandorder",
-}
-
-APP_STORE_URL = {
-    "NA": "http://itunes.apple.com/us/lookup?bundleId=com.aniplex.fategrandorder.en",
-    "CN": "http://itunes.apple.com/cn/lookup?bundleId=com.bilibili.fatego",
-    "JP": "http://itunes.apple.com/jp/lookup?bundleId=com.aniplex.fategrandorder",
-    "KR": "http://itunes.apple.com/kr/lookup?bundleId=com.netmarble.fgok",
-    "TW": "http://itunes.apple.com/tw/lookup?bundleId=com.xiaomeng.fategrandorder",
+    "CN": "https://static.biligame.com/config/fgo.config.js"
 }
 
 PLAY_STORE_XPATH_1 = "/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div/main/c-wiz[4]/div[1]/div[2]/div/div[4]/span/div/span"
@@ -34,11 +26,10 @@ VERSION_REGEX = re.compile(r"^\d+\.\d+\.\d+$")
 
 
 def get_CN_android_version():
-    r = httpx.get("https://static.biligame.com/config/fgo.config.js")
+    r = httpx.get(PLAY_STORE_URL["CN"])
     if match := re.search(r"\"latest_version\": \"(.*)\"", r.text):
         if VERSION_REGEX.match(match.group(1)):
             return match.group(1)
-
     return None
 
 
@@ -54,7 +45,7 @@ def get_play_store_ver(region: str):
             xpath_version: str = play_store_html.xpath(xpath)[0].text
             if VERSION_REGEX.match(xpath_version):
                 return xpath_version
-        except:  # pylint: disable=bare-except
+        except:
             continue
 
     for match in re.finditer(
@@ -75,29 +66,11 @@ def get_play_store_ver(region: str):
             if isinstance(deep_version, str) and VERSION_REGEX.match(deep_version):
                 return deep_version
 
-        except:  # pylint: disable=bare-except
+        except:
             pass
 
     return None
 
 
-def get_app_store_ver(region: str):
-    r = httpx.get(APP_STORE_URL[region] + f"&t={int(time.time())}")
-    app_store_version: str = r.json()["results"][0]["version"]
-    if VERSION_REGEX.match(app_store_version):
-        return app_store_version
-
-    return None
-
-
-def get_version(region: str) -> None:
-    if region not in APP_STORE_URL:
-        return None
-
-    play_store_version = get_play_store_ver(region)
-    if play_store_version is not None:
-        return play_store_version
-
-    app_store_version = get_app_store_ver(region)
-    if app_store_version is not None:
-        return app_store_version
+def get_version(region: str) -> str:
+    return get_play_store_ver(region)
